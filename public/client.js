3d47141070c48c5c9057c1c7ff10d4a5923e7589
eric nguyen <eric.tung.nguyen@gmail.com>
2018-03-22 19:43:49 -0400

    fourth

--- public/client.js
+++ public/client.js
@@ -13,7 +13,7 @@ var firstMessage=true;    // What the first message, to start on the first value
 
     socket.on('reset', function() { // on a 'reset' message clean and reste firstMessage
       firstMessage=true;
-        console.log("that worked");
+        console.log("that worked");  // for testing
       ctx.clear();
     });
 
--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,24 +1,26 @@
 #define SENSORPINA A0 // x axis
  //TODO: define other sensor inputs
 
+#define BUTTONPIN 2
+
   // potentiometer code
 int sensorPin0 = A0;    // select the input pin for the potentiometer
 int sensorValue0 = 0;  // variable to store the value coming from the sensor
 int sensorPin1 = A1;    // select the input pin for the potentiometer
 int sensorValue1 = 0;  // variable to store the value coming from the sensor
 
-const int buttonPin = 2;     // the number of the pushbutton pin
+//const int buttonPin = 2;     // the number of the pushbutton pin
 int buttonState = 0;         // variable for reading the pushbutton status
 
 
 unsigned long targetTime=0;
-const unsigned long interval=2500; //TODO: How fast should we read
+const unsigned long interval=100; //TODO: How fast should we read
 void setup(){
 // TODO: begin the serial connection with a baudrate of 115200
   Serial.begin(115200);
 
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(BUTTONPIN, INPUT);
 
   // initialize potentiometer 
   pinMode(sensorPin0, INPUT);
@@ -28,12 +30,18 @@ void setup(){
 
 void loop(){
 
-  buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(BUTTONPIN);
 
     // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    Serial.write('rst');
+    
+    Serial.println("not");
+  }
+
+  else {
+    Serial.write("rst");
+    Serial.println("");
+    Serial.println("sending the reset message");
   }
 
   
