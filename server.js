41473805ca0d8fc728939ab036e7b90ee5a13312
eric nguyen <eric.tung.nguyen@gmail.com>
2018-03-22 20:29:27 -0400

    7th

--- server.js
+++ server.js
@@ -32,8 +32,11 @@ const parser = new readLine({
 // Read data that is available on the serial port and send it to the websocket
 serial.pipe(parser);
 parser.on('data', function(data) { // on data from the arduino
-  if(data=='rst'){  // if its the 'rst' string call reset
-    io.emit('reset');
+    console.log("the data is: ", data);
+    
+    if(data=='rst'){  // if its the 'rst' string call reset
+      io.emit('reset');
+    
   }else{ // any other data we try to forward by spliting it
     var transmitData = [data.split(',')[0],data.split(',')[1]];
     io.emit('new-pos', transmitData);
